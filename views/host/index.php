d06063c91fc686a05abbd8f36c249bd5e27bf00c
Andrii Vasyliev <sol@hiqdev.com>
2015-07-29 16:02:43 +0000

    minor

--- views/host/index.php
+++ views/host/index.php
@@ -19,26 +19,24 @@ $this->breadcrumbs->setItems([
 ?>
 <?= Html::beginForm('', '') ?>
 <?php $box = ActionBox::begin(['bulk' => true, 'options' => ['class' => 'box-info']]) ?>
-<?php $box->beginActions(); ?>
+<?php $box->beginActions() ?>
 <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => Yii::t('app', 'Name server')]), ['create'], ['class' => 'btn btn-primary']) ?>&nbsp;
-<?php $box->endActions(); ?>
-<?php $box->beginBulkActions(); ?>
-<?= Html::submitButton(Yii::t('app', 'Change IP'), ['id' => 'change-ip-button', 'class' => 'btn btn-primary', 'formmethod' => 'GET', 'formaction' => Url::to('update')]); ?>
+<?php $box->endActions() ?>
+<?php $box->beginBulkActions() ?>
+<?= Html::submitButton(Yii::t('app', 'Change IP'), ['id' => 'change-ip-button', 'class' => 'btn btn-primary', 'formmethod' => 'GET', 'formaction' => Url::to('update')]) ?>
 &nbsp;
-<?= Html::submitButton(Yii::t('app', 'Delete'), ['id' => 'change-ip-button', 'class' => 'btn btn-danger', 'formmethod' => 'POST', 'formaction' => Url::to('delete')]); ?>
-<?php $box->endBulkActions(); ?>
+<?= Html::submitButton(Yii::t('app', 'Delete'), ['id' => 'change-ip-button', 'class' => 'btn btn-danger', 'formmethod' => 'POST', 'formaction' => Url::to('delete')]) ?>
+<?php $box->endBulkActions() ?>
 
-<?php $box::end(); ?>
+<?php $box::end() ?>
 
 
 <?= HostGridView::widget([
     'dataProvider' => $dataProvider,
     'filterModel'  => $searchModel,
     'columns'      => [
-        'seller_id',
-        'client_id',
-        'host',
-        'ips',
+        'host', 'ips',
+        'client_id', 'seller_id',
         'checkbox',
     ],
 ]) ?>
