76d123ab180854cb1dbf3a53a8a42b5bb9d140fa
Andrii Vasyliev <sol@hiqdev.com>
2015-07-31 14:08:48 +0000

    + ValidateFormAction

--- src/controllers/HostController.php
+++ src/controllers/HostController.php
@@ -7,12 +7,7 @@
 
 namespace hipanel\modules\domain\controllers;
 
-use hiqdev\hiart\Collection;
 use Yii;
-use yii\base\ErrorException;
-use yii\base\Exception;
-use yii\web\HttpException;
-use yii\widgets\ActiveForm;
 
 class HostController extends \hipanel\base\CrudController
 {
@@ -32,74 +27,10 @@ class HostController extends \hipanel\base\CrudController
                 'class'     => 'hipanel\actions\SmartDeleteAction',
                 'success'   => Yii::t('app', 'Name server deleted'),
             ],
+            'validate-form' => [
+                'class'     => 'hipanel\actions\ValidateFormAction',
+            ],
         ];
     }
 
-    public function actionCreateOld()
-    {
-        $model = static::newModel(['scenario' => 'create']);
-        if (Yii::$app->request->isPost) {
-            $collection = Yii::createObject([
-                'class' => Collection::className(),
-                'model' => $model,
-            ]);
-            if ($collection->load() && $collection->save())
-                return $this->redirect(['index']);
-        }
-
-        return $this->render('create', ['models' => [$model]]);
-    }
-
-    public function actionUpdateOld($id = null)
-    {
-        $condition = Yii::$app->request->get('selection') ?: $id;
-        $models = $this->findModels($condition);
-        if (Yii::$app->request->isPost) {
-            $collection = Yii::createObject([
-                'class' => Collection::className(),
-                'model' => static::newModel(['scenario' => 'update']),
-            ]);
-            $collection->load();
-            if (!$collection->validate() && Yii::$app->request->isAjax) {
-                throw new HttpException(406, $collection->firstError);
-            }
-            if ($collection->save()) {
-                if (!Yii::$app->request->isAjax) {
-                    return $this->redirect(['index']);
-                }
-            } else {
-                \yii\helpers\VarDumper::dump($collection, 10, true);die();
-            }
-        }
-
-        return $this->render('update', ['models' => $models]);
-
-    }
-
-    public function actionValidateForm($scenario)
-    {
-        $model = static::newModel(['scenario' => $scenario]);
-
-        if (Yii::$app->request->isPost) {
-            $collection = Yii::createObject([
-                'class' => Collection::className(),
-                'model' => $model,
-            ]);
-            $collection->load();
-            return $this->renderJson(ActiveForm::validateMultiple($collection->models));
-        }
-    }
-
-    public function actionDeleteOld()
-    {
-        $condition = Yii::$app->request->post('selection');
-        if (!empty($condition)) {
-            $models = $this->findModels($condition);
-            foreach ($models as $model) {
-                $model->delete();
-            }
-        }
-
-        return $this->redirect('index');
-    }
 }
