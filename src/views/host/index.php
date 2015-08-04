9b6a2a66ff8bf11281ef3625a4f3124e122441c5
Andrey Klochok <andreyklochok@gmail.com>
2015-08-04 15:38:49 +0300

    Add ActionBox to Host

--- src/views/host/index.php
+++ src/views/host/index.php
@@ -12,25 +12,30 @@ $this->breadcrumbs[] = $this->title;
 ?>
 <?= Html::beginForm() ?>
 
-<?php $box = ActionBox::begin(['bulk' => true, 'options' => ['class' => 'box-info']]) ?>
+<?php $box = ActionBox::begin(['model' => $model, 'bulk' => true, 'options' => ['class' => 'box-info']]) ?>
     <?php $box->beginActions() ?>
-        <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => Yii::t('app', 'Name server')]), ['create'], ['class' => 'btn btn-primary']) ?>&nbsp;
-        <?= Html::a(Yii::t('app', 'Advanced search'), '#', ['class' => 'btn btn-info search-button']) ?>
+        <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => Yii::t('app', 'Name server')]), ['create'], ['class' => 'btn btn-primary']) ?>
+        &nbsp;
+        <?= $box->renderSearchButton() ?>
     <?php $box->endActions() ?>
     <?php $box->beginBulkActions() ?>
-        <?= Html::submitButton(Yii::t('app', 'Change IP'), ['id' => 'change-ip-button', 'class' => 'btn btn-primary', 'formmethod' => 'GET', 'formaction' => Url::to('update')]) ?>
-        &nbsp;
-        <?= Html::submitButton(Yii::t('app', 'Delete'), ['id' => 'delete-button', 'class' => 'btn btn-danger', 'formmethod' => 'POST', 'formaction' => Url::to('delete')]) ?>
+        <?= $box->renderBulkButton(Yii::t('app', 'Change IP'), Url::to('update')) ?>
+        <?= $box->renderBulkButton(Yii::t('app', 'Delete'), Url::to('delete')) ?>
     <?php $box->endBulkActions() ?>
+    <?= $box->renderSearchForm() ?>
 <?php $box::end() ?>
 
 <?= HostGridView::widget([
     'dataProvider' => $dataProvider,
     'filterModel'  => $searchModel,
     'columns'      => [
-        'host', 'ips', 'domain',
-        'client_id', 'seller_id',
         'checkbox',
+        'host',
+        'ips',
+        'domain',
+        'client_id',
+        'seller_id',
+
     ],
 ]) ?>
 
