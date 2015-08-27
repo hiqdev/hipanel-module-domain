1152284fe224673d6377ec736d0b6a06c3eab012
Andrii Vasyliev <sol@hiqdev.com>
2015-08-27 16:17:06 +0000

    + details/edit buttons at domain contacts

--- src/views/domain/_contactsTables.php
+++ src/views/domain/_contactsTables.php
@@ -1,16 +1,27 @@
 <?php
-use yii\widgets\DetailView;
+
 use hipanel\modules\domain\models\Domain;
+use hipanel\widgets\Box;
 use yii\helpers\Html;
+use yii\widgets\DetailView;
 
 ?>
 
-
+<br/><br/>
 <?php foreach (Domain::$contactOptions as $item) : ?>
+    <?php $contact = $domainContactInfo[$item] ?>
     <div class="col-md-6">
-        <?= Html::tag('h3', ucfirst($item) . ' ' . Yii::t('app', 'contact')); ?>
+        <div class="row">
+            <div class="col-md-6"><?= Html::tag('h4', Yii::t('app', ucfirst($item) . ' contact')) ?></div>
+            <div class="col-md-6">
+                <div class="pull-right btn-group" style="padding-top:10px">
+                    <?= Html::a(Yii::t('app', 'Details'),   ['@contact/view',   'id' => $contact['id']], ['class' => 'btn btn-default btn-xs']) ?>
+                    <?= Html::a(Yii::t('app', 'Edit'),      ['@contact/update', 'id' => $contact['id']], ['class' => 'btn btn-default btn-xs']) ?>
+                </div>
+            </div>
+        </div>
         <?= DetailView::widget([
-            'model' => $domainContactInfo[$item],
+            'model' => $contact,
             'attributes' => [
                 'name',
                 'email',
@@ -18,6 +29,6 @@ use yii\helpers\Html;
                 'voice_phone',
                 'fax_phone',
             ]
-        ]); ?>
+        ]) ?>
     </div>
 <?php endforeach; ?>
