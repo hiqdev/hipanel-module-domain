0d5594bbd75aa5bc8a1b922abe3c0f9a0874edd6
Andrey Klochok <andreyklochok@gmail.com>
2015-10-23 17:28:35 +0300

    Work on DomainProduct

--- src/models/DomainProduct.php
+++ src/models/DomainProduct.php
@@ -6,5 +6,28 @@ use hipanel\models\CartPosition;
 
 class DomainProduct extends CartPosition
 {
+    /**
+     * @var Domain
+     */
+    public $model;
 
+    public $icon = '<i class="fa fa-server text-muted"></i>';
+
+    public $operation;
+
+    public function getName() {
+        return $this->model->domain;
+    }
+
+    public function getDescription() {
+        return "Покупка домена " . $this->model->domain;
+    }
+
+    public function getScenario() {
+        return 'product';
+    }
+
+    public function setScenario($scenario) {
+
+    }
 }
