5e63c79415b5e5a793c836fba1ddb37e02c3f597
Andrey Klochok <andreyklochok@gmail.com>
2015-11-24 15:37:26 +0200

    NSync plugin complete

--- src/assets/js/NSyncPluginAsset.js
+++ src/assets/js/NSyncPluginAsset.js
@@ -2,22 +2,27 @@
  * Created by tofid on 23.11.15.
  */
 
-;(function ($, window, document, undefined) {
+;
+(function ($, window, document, undefined) {
     var pluginName = "NSync";
 
     var defaults = {
-        inlineFieldSelector: '.inline-form-selector',
+        inlineFieldSelector: '.inline-form-selector input',
         dynamicFieldSelector: '.dynamic-form-selector',
 
         dynamicFormWidgetContainer: '.dynamicform_wrapper',
         dynamicFormWidgetItem: '.item',
         dynamicFormWidgetInsertButton: '.add-item',
         dynamicFormWidgetDeleteButton: '.remove-item',
-
-        state: []
+        dynamicFormWidgetLimit: 15
     };
 
     function Plugin(element, options) {
+        this.element = element;
+        this.state = [];
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
         this.init();
     }
 
@@ -25,104 +30,131 @@
         // Common
         init: function (event) {
             this.addInlineFormListener(event);
-            //this.addDynamicFormListener();
+            this.addDynamicFormListener(event);
         },
-        stateIsChanged: function () {
+        isStateChanged: function () {
             return this.getDynamicElementsCount() !== this.getInlineElementsCount();
         },
 
         // Inline Processing
         addInlineFormListener: function (event) {
-            var _ = this;
-            $(defaults.inlineFieldSelector).find('input').eq(0).on('keyup change', function (event) {
-                _.updateInlineForm(event);
+            var that = this;
+            $(this.element).find(this.options.inlineFieldSelector).on('change keyup input', function (event) {
+                $(this).data('touched', 1);
+                that.updateDynamicForm(event);
+                $(this).data('touched', 0);
             });
         },
         updateInlineForm: function (event) {
-            this.getStateFromInlineForm();
-            this.drawDynamicForm(event);
+            this.getStateFromDynamicForm();
+            this.drawInlineForm(event);
         },
-        drawInlineForm: function () {
+        drawInlineForm: function (event) {
+            var nsInlineField = '';
+            this.state.forEach(function (elem, i) {
+                nsInlineField += elem.name;
+                if (elem.ip !== '') {
+                    nsInlineField +=  '/' + elem.ip;
+                }
+                nsInlineField += ', ';
+            });
 
+            $(this.element).find(this.options.inlineFieldSelector).val(nsInlineField.replace(/,\s*$/g, ''));
         },
         getInlineElementsCount: function () {
             return this.parseInlineForm().length;
         },
         getStateFromInlineForm: function () {
-            defaults.state = this.parseInlineForm();
+            this.state = this.parseInlineForm();
         },
         parseInlineForm: function () {
             var items = [],
                 wholeStrSplitRe = /,\s*/,
                 ipStrSplitRe = /\/\s*/,
-                splits = $(defaults.inlineFieldSelector).find('input').eq(0).val().split(wholeStrSplitRe);
-            splits.forEach(function(element, index, array) {
-                var split = element.split(ipStrSplitRe);
+                splits = $(this.element).find(this.options.inlineFieldSelector).val().split(wholeStrSplitRe);
+            splits.forEach(function (elem, i) {
+                var split = elem.split(ipStrSplitRe);
                 items.push({
                     name: split[0],
                     ip: split[1]
                 });
             });
+
             return items;
         },
 
         // Dynamic Processing
         addDynamicFormListener: function (event) {
-
+            var that = this;
+            $(this.element).find(this.options.dynamicFormWidgetContainer).on('afterDelete afterInsert', function (event) {
+                if (!that.element.find(that.options.inlineFieldSelector).data('touched')) {
+                    that.updateInlineForm(event);
+                }
+            });
+            $(this.element).find(this.options.dynamicFormWidgetContainer).on('change keyup input', 'input', function (event) {
+                that.updateInlineForm(event);
+            });
         },
         updateDynamicForm: function () {
-
+            this.getStateFromInlineForm();
+            this.drawDynamicForm(event);
         },
         drawDynamicForm: function (event) {
-            if (this.stateIsChanged()) {
-                var diff = 0,
-                    dynamicElems = this.getDynamicElementsCount(),
-                    inlineElems = this.getInlineElementsCount();
-                // Draw items
-                if (dynamicElems < inlineElems) {
-                    diff = inlineElems - dynamicElems;
-                    for (var i = 0; i < diff; i++) {
+            var that = this;
+            // Draw dynamic form containers
+            if (this.isStateChanged()) {
+                var diff = this.getInlineElementsCount() - this.getDynamicElementsCount();
+
+                for (var i = 0; i < Math.abs(diff); i++) {
+                    if (diff > 0) {
                         this.addDynamicItem(event);
-                    }
-                } else {
-                    diff = dynamicElems - inlineElems;
-                    for (var i = 0; i < diff; i++) {
+                    } else {
                         this.deleteDynamicItem(event);
                     }
                 }
             }
-            // Fill the items
-            defaults.state.forEach(function(element, index, array) {
-                var DWContainer = $(defaults.dynamicFormWidgetContainer);
-                var state = defaults.state;
-                DWContainer.find(defaults.dynamicFormWidgetItem).each(function(index, element) {
-                    var stateItem = state.shift(),
-                        containerFields = $(element).find('input');
-                    containerFields.eq(0).val(stateItem.name);
-                    containerFields.eq(1).val(stateItem.ip);
+            // Fill dynamic form widget fields
+            var state = $.extend(true, [], this.state);
+            var DWContainer = $(this.element).find(that.options.dynamicFormWidgetContainer);
+            DWContainer.find(that.options.dynamicFormWidgetItem).each(function (index, element) {
+                var stateItem = state.shift();
+                var containerFields = $(element).find('input');
+                containerFields.eq(0).val(stateItem.name);
+                containerFields.eq(1).val(stateItem.ip);
+            });
+        },
+        getDynamicElementsCount: function () {
+            return $(this.element).find(this.options.dynamicFormWidgetContainer).find(this.options.dynamicFormWidgetItem).length;
+        },
+        getStateFromDynamicForm: function () {
+            this.state = this.parseDynamicForm();
+        },
+        parseDynamicForm: function () {
+            var items = [];
+            var DWContainer = $(this.element).find(this.options.dynamicFormWidgetContainer);
+            DWContainer.find(this.options.dynamicFormWidgetItem).each(function (i, elem) {
+                var containerFields = $(elem).find('input');
+                items.push({
+                    name: containerFields.eq(0).val(),
+                    ip: containerFields.eq(1).val()
                 });
             });
 
-        },
-        getDynamicElementsCount: function () {
-            return $('.item').eq(0).closest(defaults.dynamicFormWidgetContainer).find(defaults.dynamicFormWidgetItem).length;
-        },
-        getStateFromDynamicForm: function () {
-
+            return items;
         },
         addDynamicItem: function (event) {
-            var df = $(defaults.dynamicFormWidgetContainer);
+            var df = $(this.element).find(this.options.dynamicFormWidgetContainer);
             var df_options = window[df.data('dynamicform')];
-            df.yiiDynamicForm("addItem", df_options, event, $(defaults.dynamicFormWidgetInsertButton).eq(-1));
+            df.yiiDynamicForm("addItem", df_options, event, $(df).find(this.options.dynamicFormWidgetInsertButton).eq(-1));
         },
         deleteDynamicItem: function (event) {
-            var df = $(defaults.dynamicFormWidgetContainer);
+            var df = $(this.element).find(this.options.dynamicFormWidgetContainer);
             var df_options = window[df.data('dynamicform')];
-            df.yiiDynamicForm("deleteItem", df_options, event, $(defaults.dynamicFormWidgetDeleteButton).eq(-1));
+            df.yiiDynamicForm("deleteItem", df_options, event, $(df).find(this.options.dynamicFormWidgetDeleteButton).eq(-1));
         }
     };
 
-    $.fn[ pluginName ] = function (options) {
+    $.fn[pluginName] = function (options) {
         this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
                 $.data(this, "plugin_" + pluginName, new Plugin(this, options));
