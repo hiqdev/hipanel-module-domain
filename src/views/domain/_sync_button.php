db842ecfb1bf313e23e608752dc321cac0107ffa
Andrey Klochok <andreyklochok@gmail.com>
2015-12-25 13:45:38 +0200

    Add indent in _sync_button

--- src/views/domain/_sync_button.php
+++ src/views/domain/_sync_button.php
@@ -16,7 +16,7 @@ use yii\web\JsExpression;
 Pjax::begin(array_merge(Yii::$app->params['pjax'], ['id' => 'domain-view-sync-button']));
 
 echo Html::a(
-    '<i class="ion-ios-loop-strong"></i>' . Yii::t('app', 'Synchronize contacts'),
+    '<i class="ion-ios-loop-strong"></i> ' . Yii::t('app', 'Synchronize contacts'),
     ['sync', 'id' => $model->id],
     [
         'id' => 'test123',
