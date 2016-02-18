e9fe7e1b7a6899c2b864f3d5c6e4d95ea6702cbc
Andrii Vasyliev <sol@hiqdev.com>
2016-02-18 14:16:53 +0000

    translations

--- src/messages/ru/domain.php
+++ src/messages/ru/domain.php
@@ -30,4 +30,7 @@ return [
 
     'Starting the transfer procedure for the following domains' => 'Начинаем процедуру трансфера для следующих доменов',
     'Return to transfer form' => 'Вернуться к форме трансфера',
+
+    'Domain was successfully pushed' => 'Домен был передан успешно',
+    'Failed to push the domain' => 'Не удалось передать домен',
 ];
