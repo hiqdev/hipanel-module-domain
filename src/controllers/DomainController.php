5ff76febaf2f0e539449c9e6170477c7f589cc04
Andrey Klochok <andreyklochok@gmail.com>
2016-03-17 18:05:29 +0200

    minor

--- src/controllers/DomainController.php
+++ src/controllers/DomainController.php
@@ -249,7 +249,6 @@ class DomainController extends \hipanel\base\CrudController
                     'success' => [
                         'class' => ProxyAction::class,
                         'action' => 'index'
-                    ],
                 ],
                 'on beforeFetch' => function (Event $event) {
                     /** @var \hipanel\actions\SearchAction $action */
