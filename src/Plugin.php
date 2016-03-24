d363819a0ef4faa81ef60860895248dddd14952e
Andrey Klochok <andreyklochok@gmail.com>
2016-03-24 17:32:36 +0200

    minor

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,7 +9,7 @@
  * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
  */
 
-namespace hipanel\modules\domain;
+namespace hipanel\modules\domainchecker;
 
 class Plugin extends \hiqdev\pluginmanager\Plugin
 {
