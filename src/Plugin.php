8cd44dae96782cda41bdc10d4e310fbabb25e50a
Andrey Klochok <andreyklochok@gmail.com>
2016-03-24 13:27:41 +0200

    Add translation

---
+++ src/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Domain checker plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-domain-checker
+ * @package   hipanel-domain-checker
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\domain;
+
+class Plugin extends \hiqdev\pluginmanager\Plugin
+{
+    protected $_items = [
+        'aliases' => [
+            '@domainchecker' => '/domainchecker/domainchecker',
+        ],
+        'modules' => [
+            'domainchecker' => [
+                'class' => 'hipanel\modules\domainchecker\Module',
+            ],
+        ],
+        'components' => [
+            'i18n' => [
+                'translations' => [
+                    'hipanel/domainchecker' => [
+                        'class' => 'yii\i18n\PhpMessageSource',
+                        'forceTranslation' => true,
+                        'basePath' => '@hipanel/modules/domainchecker/messages',
+                        'fileMap' => [
+                            'hipanel/domainchecker' => 'domainchecker.php',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+}
