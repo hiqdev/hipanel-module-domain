f5ebe6e86a8fc1a03155083f437ecfe69a64e4e9
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 14:14:15 +0000

    used composer-config-plugin

---
+++ src/config/hisite.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Domain plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-domain
+ * @package   hipanel-module-domain
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'aliases' => [
+        '@domain' => '/domain/domain',
+        '@host'   => '/domain/host',
+    ],
+    'modules' => [
+        'domain' => [
+            'class' => \hipanel\modules\domain\Module::class,
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'hipanel/domain' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'forceTranslation' => true,
+                    'basePath' => '@hipanel/modules/domain/messages',
+                    'fileMap' => [
+                        'hipanel/domain' => 'domain.php',
+                    ],
+                ],
+            ],
+        ],
+        'menuManager' => [
+            'menus' => [
+                'domain' => \hipanel\modules\domain\SidebarMenu::class,
+            ],
+        ],
+    ],
+];
