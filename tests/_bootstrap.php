35ff0d1b95701a79855136c9e99472afe2f4371b
Andrii Vasyliev <sol@hiqdev.com>
2016-08-08 17:39:54 +0300

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * HiSite Domain module
+ *
+ * @link      https://github.com/hiqdev/hipanel-domain-checker
+ * @package   hipanel-domain-checker
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
