5e5ca213c8b5bda61563838ff15ce39ff787e779
Andrii Vasyliev <sol@hiqdev.com>
2016-11-11 11:30:50 +0000

    added DomainBulkActionsMenu

--- src/views/domain/index.php
+++ src/views/domain/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use hipanel\modules\domain\menus\DomainBulkActionsMenu;
 use hipanel\modules\domain\grid\DomainGridView;
 use hipanel\widgets\ActionBox;
 use hipanel\widgets\AjaxModal;
@@ -46,30 +47,10 @@ CSS
             <?= Yii::t('hipanel', 'Basic actions') ?>
             <span class="caret"></span>
         </button>
-        <?= Dropdown::widget([
+        <?= DomainBulkActionsMenu::create()->render([
+            'class' => Dropdown::class,
             'encodeLabels' => false,
-            'items' => [
-                ['label' => Yii::t('hipanel/domain', 'Sync contacts'), 'url' => '#', 'linkOptions' => ['data-action' => 'sync'], 'visible' => Yii::$app->user->can('support')],
-                ['label' => Yii::t('hipanel/domain', 'Renew'), 'url' => '#', 'linkOptions' => ['data-action' => 'bulk-renewal']],
-                ['label' => Yii::t('hipanel/domain', 'Push domain'), 'url' => '#bulk-domain-push-modal', 'linkOptions' => ['data-toggle' => 'modal']],
-                // Hold
-                '<li role="presentation" class="divider"></li>',
-                ['label' => '<i class="fa fa-toggle-on"></i> ' . Yii::t('hipanel/domain', 'Enable Hold'), 'url' => '#', 'linkOptions' => ['data-action' => 'enable-hold'], 'visible' => Yii::$app->user->can('support')],
-                ['label' => '<i class="fa fa-toggle-off"></i> ' . Yii::t('hipanel/domain', 'Disable Hold'), 'url' => '#', 'linkOptions' => ['data-action' => 'disable-hold'], 'visible' => Yii::$app->user->can('support')],
-                // WHOIS protect
-                '<li role="presentation" class="divider"></li>',
-                ['label' => '<i class="fa fa-toggle-on"></i> ' . Yii::t('hipanel/domain', 'Enable WHOIS protect'), 'url' => '#', 'linkOptions' => ['data-action' => 'enable-whois-protect']],
-                ['label' => '<i class="fa fa-toggle-off"></i> ' . Yii::t('hipanel/domain', 'Disable WHOIS protect'), 'url' => '#', 'linkOptions' => ['data-action' => 'enable-whois-protect']],
-                // Lock
-                '<li role="presentation" class="divider"></li>',
-                ['label' => '<i class="fa fa-toggle-on"></i> ' . Yii::t('hipanel/domain', 'Enable Lock'), 'url' => '#', 'linkOptions' => ['data-action' => 'enable-lock']],
-                ['label' => '<i class="fa fa-toggle-off"></i> ' . Yii::t('hipanel/domain', 'Disable Lock'), 'url' => '#', 'linkOptions' => ['data-action' => 'disable-lock']],
-                // Autorenew
-                '<li role="presentation" class="divider"></li>',
-                ['label' => '<i class="fa fa-toggle-on"></i> ' . Yii::t('hipanel/domain', 'Enable autorenew'), 'url' => '#', 'linkOptions' => ['data-action' => 'enable-autorenewal']],
-                ['label' => '<i class="fa fa-toggle-off"></i> ' . Yii::t('hipanel/domain', 'Disable autorenew'), 'url' => '#', 'linkOptions' => ['data-action' => 'disable-autorenewal']],
-            ]
-        ]); ?>
+        ]) ?>
     </div>
     <?= AjaxModal::widget([
         'id' => 'bulk-domain-push-modal',
