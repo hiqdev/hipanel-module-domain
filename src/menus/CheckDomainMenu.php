6ca7999de59478986770a0bf0fafe3b5d1d854e4
Andrii Vasyliev <sol@hiqdev.com>
2016-11-16 09:03:07 +0000

    + CheckDomainMenu

---
+++ src/menus/CheckDomainMenu.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Domain plugin for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-domain
+ * @package   hipanel-module-domain
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hipanel\modules\domain\menus;
+
+use Yii;
+
+class CheckDomainMenu extends \hiqdev\menumanager\Menu
+{
+    public function items()
+    {
+        return [
+            'check-domain' => [
+                'label' => $this->renderView('checkDomain'),
+                'encode' => false,
+            ],
+        ];
+    }
+}
