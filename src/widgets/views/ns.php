e4f52251e82a540bf654f49aa2596167a51927aa
tofid <andreyklochok@gmail.com>
2017-04-06 19:22:42 +0300

    Changed info block color from info to warning in NS tab widget

--- src/widgets/views/ns.php
+++ src/widgets/views/ns.php
@@ -46,130 +46,128 @@ $(document).on('pjax:complete', function(event) {
         'data-pjaxPush' => false,
     ],
 ]); ?>
-    <?php if (!is_array($model)) : ?>
-        <?= Html::activeHiddenInput($model, 'id') ?>
-        <?= Html::activeHiddenInput($model, 'domain') ?>
-    <?php else : ?>
-        <?php foreach ($model as $item) : ?>
-            <?= Html::activeHiddenInput($item, "[$item->id]id") ?>
-            <?= Html::activeHiddenInput($item, "[$item->id]domain") ?>
-        <?php endforeach; ?>
-    <?php endif; ?>
-    <div class="alert alert-info alert-dismissible fade in" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+<?php if (!is_array($model)) : ?>
+    <?= Html::activeHiddenInput($model, 'id') ?>
+    <?= Html::activeHiddenInput($model, 'domain') ?>
+<?php else : ?>
+    <?php foreach ($model as $item) : ?>
+        <?= Html::activeHiddenInput($item, "[$item->id]id") ?>
+        <?= Html::activeHiddenInput($item, "[$item->id]domain") ?>
+    <?php endforeach; ?>
+<?php endif; ?>
+<div class="callout callout-warning">
+    <h4><i class="fa fa-info-circle"></i>&nbsp;&nbsp;<?= Yii::t('hipanel', 'Notice') ?></h4>
+    <p>
+        <?= Yii::t('hipanel:domain', 'With this form you can assign the authoritative name servers for your domain.') ?>
+        <br>
+        <?= Yii::t('hipanel:domain', 'IP addresses can be assigned to child name servers only.') ?>
+        <?= Yii::t('hipanel:domain', 'Child name servers are created or changed automatically according to specified data.') ?>
+    </p>
+</div>
 
-        <h4><i class="fa fa-info-circle"></i>&nbsp;&nbsp;<?= Yii::t('hipanel', 'Notice') ?></h4>
-
-        <p>
-            <?= Yii::t('hipanel:domain', 'With this form you can assign the authoritative name servers for your domain.') ?></p><p>
-            <?= Yii::t('hipanel:domain', 'IP addresses can be assigned to child name servers only.') ?>
-            <?= Yii::t('hipanel:domain', 'Child name servers are created or changed automatically according to specified data.') ?>
-        </p>
-    </div>
-
-    <div class="row" style="margin-top: 15pt;">
-        <div class="col-md-10 inline-form-selector">
-            <?php if (!is_array($model)) : ?>
-                <?= Html::activeTextInput($model, 'nsips', [
-                    'class' => 'form-control',
-                    'placeholder' => $model->getAttributeLabel('nameservers'),
-                    'autocomplete' => 'off',
-                ]) ?>
-            <?php else : ?>
-                <?= Html::textInput('nsips', '', [
-                    'class' => 'form-control',
-                    'placeholder' => reset($model)->getAttributeLabel('nameservers'),
-                    'autocomplete' => 'off',
-                ]) ?>
-            <?php endif; ?>
-        </div>
-        <div class="col-md-2 text-right">
-            <?= Html::submitButton(Yii::t('hipanel', 'Save'), [
-                'class' => 'btn btn-success',
-                'id' => 'nss-save-button',
-                'data-loading-text' => '<i class="fa fa-circle-o-notch fa-spin"></i> ' . Yii::t('hipanel', 'saving'),
+<div class="row" style="margin-top: 15pt;">
+    <div class="col-md-10 inline-form-selector">
+        <?php if (!is_array($model)) : ?>
+            <?= Html::activeTextInput($model, 'nsips', [
+                'class' => 'form-control',
+                'placeholder' => $model->getAttributeLabel('nameservers'),
+                'autocomplete' => 'off',
             ]) ?>
-        </div>
+        <?php else : ?>
+            <?= Html::textInput('nsips', '', [
+                'class' => 'form-control',
+                'placeholder' => reset($model)->getAttributeLabel('nameservers'),
+                'autocomplete' => 'off',
+            ]) ?>
+        <?php endif; ?>
     </div>
+    <div class="col-md-2 text-right">
+        <?= Html::submitButton(Yii::t('hipanel', 'Save'), [
+            'class' => 'btn btn-success',
+            'id' => 'nss-save-button',
+            'data-loading-text' => '<i class="fa fa-circle-o-notch fa-spin"></i> ' . Yii::t('hipanel', 'saving'),
+        ]) ?>
+    </div>
+</div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <hr>
+<div class="row">
+    <div class="col-md-12">
+        <hr>
+        <div class="">
             <div class="">
-                <div class="">
-                    <?php DynamicFormWidget::begin([
-                        'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
-                        'widgetBody' => '.container-items', // required: css class selector
-                        'widgetItem' => '.item', // required: css class
-                        'limit' => 13, // the maximum times, an element can be cloned (default 999)
-                        'min' => 1, // 0 or 1 (default 1)
-                        'insertButton' => '.add-item', // css class
-                        'deleteButton' => '.remove-item', // css class
-                        'model' => reset($nsModels),
-                        'formId' => 'nss-form-pjax',
-                        'formFields' => [
-                            'name',
-                            'ip',
-                            'domain_name',
-                        ],
-                    ]) ?>
+                <?php DynamicFormWidget::begin([
+                    'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
+                    'widgetBody' => '.container-items', // required: css class selector
+                    'widgetItem' => '.item', // required: css class
+                    'limit' => 13, // the maximum times, an element can be cloned (default 999)
+                    'min' => 1, // 0 or 1 (default 1)
+                    'insertButton' => '.add-item', // css class
+                    'deleteButton' => '.remove-item', // css class
+                    'model' => reset($nsModels),
+                    'formId' => 'nss-form-pjax',
+                    'formFields' => [
+                        'name',
+                        'ip',
+                        'domain_name',
+                    ],
+                ]) ?>
 
-                    <div class="container-items">
-                        <?php foreach ($nsModels as $i => $nsModel): ?>
-                            <?= Html::activeHiddenInput($nsModel, "[$i]domain_name", ['value' => $model->domain, 'class' => 'domain_name']) ?>
-                            <div class="item">
-                                <div class="row" style="margin-bottom: 5pt">
-                                    <div class="col-md-5">
-                                        <?= $form->field($nsModel, "[$i]name")->textInput([
-                                            'placeholder' => $nsModel->getAttributeLabel('name'),
-                                            'data-attribute' => 'name',
-                                        ])->label(false) ?>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <?php if (!is_array($model)) : ?>
-                                            <?= $form->field($nsModel, "[$i]ip")->widget(StaticCombo::class, [
-                                                'formElementSelector' => '.item',
-                                                'inputOptions' => [
-                                                    'disabled' => !StringHelper::endsWith($nsModel->name, $model->domain),
-                                                    'data-attribute' => 'ip',
-                                                ],
-                                                'data' => $nsModel->ip ? array_combine($nsModel->ip, $nsModel->ip) : [],
-                                                'multiple' => true,
-                                                'pluginOptions' => [
-                                                    'select2Options' => [
-                                                        'tags' => true,
-                                                        'tokenSeparators' => [';', ',', ' '],
-                                                        'minimumResultsForSearch' => new JsExpression('Infinity'),
-                                                        'placeholder' => $nsModel->getAttributeLabel('ip'),
-                                                        'language' => [
-                                                            'noResults' => new JsExpression("function (params) {
+                <div class="container-items">
+                    <?php foreach ($nsModels as $i => $nsModel): ?>
+                        <?= Html::activeHiddenInput($nsModel, "[$i]domain_name", ['value' => $model->domain, 'class' => 'domain_name']) ?>
+                        <div class="item">
+                            <div class="row" style="margin-bottom: 5pt">
+                                <div class="col-md-5">
+                                    <?= $form->field($nsModel, "[$i]name")->textInput([
+                                        'placeholder' => $nsModel->getAttributeLabel('name'),
+                                        'data-attribute' => 'name',
+                                    ])->label(false) ?>
+                                </div>
+                                <div class="col-md-5">
+                                    <?php if (!is_array($model)) : ?>
+                                        <?= $form->field($nsModel, "[$i]ip")->widget(StaticCombo::class, [
+                                            'formElementSelector' => '.item',
+                                            'inputOptions' => [
+                                                'disabled' => !StringHelper::endsWith($nsModel->name, $model->domain),
+                                                'data-attribute' => 'ip',
+                                            ],
+                                            'data' => $nsModel->ip ? array_combine($nsModel->ip, $nsModel->ip) : [],
+                                            'multiple' => true,
+                                            'pluginOptions' => [
+                                                'select2Options' => [
+                                                    'tags' => true,
+                                                    'tokenSeparators' => [';', ',', ' '],
+                                                    'minimumResultsForSearch' => new JsExpression('Infinity'),
+                                                    'placeholder' => $nsModel->getAttributeLabel('ip'),
+                                                    'language' => [
+                                                        'noResults' => new JsExpression("function (params) {
                                                                 return " . Json::encode(Yii::t('hipanel:domain', 'Up to 13 IPv4 or IPv6 addresses separated with comma')) . ";
                                                             }")
-                                                        ]
-                                                    ],
+                                                    ]
                                                 ],
-                                            ])->label(false) ?>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="col-md-2 text-right">
-                                        <div class="btn-group" role="group">
-                                            <button type="button" class="add-item btn btn-default"><i
+                                            ],
+                                        ])->label(false) ?>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="col-md-2 text-right">
+                                    <div class="btn-group" role="group">
+                                        <button type="button" class="add-item btn btn-default"><i
                                                     class="glyphicon glyphicon-plus"></i></button>
-                                            <button type="button" class="remove-item btn btn-default"><i
+                                        <button type="button" class="remove-item btn btn-default"><i
                                                     class="glyphicon glyphicon-minus"></i></button>
-                                        </div>
                                     </div>
                                 </div>
-                                <?= Html::activeHiddenInput($nsModel, "[$i]domain_name", ['value' => $model->domain]) ?>
                             </div>
+                            <?= Html::activeHiddenInput($nsModel, "[$i]domain_name", ['value' => $model->domain]) ?>
+                        </div>
 
-                        <?php endforeach; ?>
-                    </div>
-                    <?php DynamicFormWidget::end(); ?>
+                    <?php endforeach; ?>
                 </div>
+                <?php DynamicFormWidget::end(); ?>
             </div>
         </div>
     </div>
+</div>
 
 <?php ActiveForm::end(); ?>
 <?php Pjax::end(); ?>
