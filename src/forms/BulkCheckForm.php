6edd6cf08ed99e4699cab3c295e543f24edee284
tofid <andreyklochok@gmail.com>
2017-06-27 19:55:00 +0300

    Done with BulkCheckForm model

--- src/forms/BulkCheckForm.php
+++ src/forms/BulkCheckForm.php
@@ -3,27 +3,160 @@
 namespace hipanel\modules\domain\forms;
 
 use hipanel\modules\dns\validators\DomainPartValidator;
+use hipanel\modules\domain\logic\DomainVariationsGenerator;
 use Yii;
+use yii\base\Model;
+use yii\helpers\StringHelper;
 
-class BulkCheckForm extends CheckForm
+class BulkCheckForm extends Model
 {
+    public $fqdns;
+
+    public $zones;
+    /**
+     * @var array
+     */
+    private $availableZones;
+
+    /**
+     * BulkCheckForm constructor.
+     * @param array $availableZones
+     * @param array $config
+     */
+    public function __construct($availableZones, array $config = [])
+    {
+        parent::__construct($config);
+
+        $this->availableZones = $availableZones;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['fqdn'], DomainPartValidator::class, 'enableIdn' => true, 'mutateAttribute' => false, 'message' => Yii::t('hipanel:domain', 'Domain name is invalid')],
-            [['fqdn'], 'zoneIsAllowed'],
-            [['fqdn'], 'required'],
+            // FQDNs is required field
+            [['fqdns'], 'required'],
+
+            // Split FQDNs
+            [['fqdns'], 'filter', 'filter' => function ($fqdns) {
+                $domains = [];
+                foreach (StringHelper::explode($fqdns, "\n") as $line) {
+                    $domains = $domains + preg_split('/([,;\s])/i', $line);
+                }
+
+                return $domains;
+            }],
+
+            // Apply DomainPartValue validator to the split FQDNs
+            [['fqdns'], 'each', 'rule' => [DomainPartValidator::class]],
+            [['fqdns'], 'filter', 'filter' => function ($value) {
+                return array_map('mb_strtolower', $value);
+            }],
+
+            // Check split FQDNs to zone is allowed
+            [['fqdns'], 'filter', 'filter' => function ($value) {
+                return array_filter($value, function ($value) {
+                    list(, $zone) = explode('.', $value, 2);
+
+                    if (empty($zone)) {
+                        return $value;
+                    }
+
+                    return in_array($zone, array_keys($this->availableZones), true);
+                });
+            }],
+
+            // Check if zones is available
+            [['zones'], 'filter', 'filter' => function ($zones) {
+                return array_filter((array)$zones, function ($zone) {
+                    return in_array($zone, array_keys($this->availableZones), true);
+                });
+            }, 'skipOnEmpty' => true],
         ];
+    }
+
+    public function getFqdnsInline()
+    {
+        return $this->fqdns ? implode(' ', $this->fqdns) : '';
+    }
+
+    public function getData()
+    {
+        $result = [];
+        $ifSingleZone = !empty($this->zones) && count($this->zones) == 1 ? reset($this->zones) : null;
+        foreach ($this->fqdns as $row) {
+            $result[] = ['fqdn' => $row, 'zone' => $ifSingleZone];
+        }
+
+        return $result;
     }
 
     public function attributeLabels()
     {
         return [
-            'fqdn' => Yii::t('hipanel:domain', 'Domains'),
-            'zone' => Yii::t('hipanel:domain', 'Zones'),
+            'fqdns' => Yii::t('hipanel:domain', 'Domains'),
+            'zones' => Yii::t('hipanel:domain', 'Zones'),
         ];
+    }
+
+    public function adjustZonesFor(CheckForm $checkForm)
+    {
+        // Single domain, no zones selected
+        //  => all zones
+        if (count($this->fqdns) === 1 && count($this->zones) === 0) {
+            return $this->availableZones;
+        }
+
+        // Single domain and single zone
+        //  => all zones ordered by the selected zone
+        if (count($this->fqdns) === 1 && count($this->zones) === 1) {
+            return $this->getAvailableZonesOrderedBy('.' . reset($this->zones));
+        }
+
+        // Many domains or many zones
+        //  => Intersection between selected and available zones
+        $zones = array_intersect_key($this->availableZones, array_flip((array)$this->zones));
+
+        // In case there is a zone in the domain
+        //  => Add it to the zones array
+        if ($checkForm->getZone()) {
+            $zones[$checkForm->getZone()] = $checkForm->availableZones[$checkForm->getZone()];
+        }
+
+        // In case there are no selected zones and domain does not contain zone
+        //  => all zones
+        if (empty($zones)) {
+            $zones = $this->availableZones;
+        }
+
+        return $zones;
+    }
+
+    public function variate($fqdn)
+    {
+        $form = new CheckForm($this->availableZones);
+        $form->fqdn = $fqdn;
+
+        $generator = new DomainVariationsGenerator($form->getDomain(), $form->getZone(), $this->adjustZonesFor($form));
+        return $generator->run();
+    }
+
+    private function getAvailableZonesOrderedBy($zone)
+    {
+        $zones = array_diff($this->availableZones, [$zone]);
+        return [reset($this->zones) => $zone] + $zones;
+    }
+
+    public function variateAll()
+    {
+        $results = [];
+
+        foreach ($this->fqdns as $fqdn) {
+            $results = array_merge($results, $this->variate($fqdn));
+        }
+
+        return $results;
     }
 }
