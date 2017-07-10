f693a8f026556f9793a4468b66c24fbc5558eb5e
tofid <andreyklochok@gmail.com>
2017-07-10 15:43:52 +0300

    Fixed sidebar search domain form

--- src/views/menus/checkDomain.php
+++ src/views/menus/checkDomain.php
@@ -7,7 +7,7 @@ use yii\helpers\Url;
 <?php $searchUrl = Url::to('@domain-check/check-domain') ?>
 <form action="<?= $searchUrl ?>" method="get" class="sidebar-form">
     <div class="input-group">
-        <input type="text" name="fqdn" class="form-control" placeholder="<?= Yii::t('hipanel:domain', 'Check domain') ?>..."/>
+        <input type="text" name="fqdns" class="form-control" placeholder="<?= Yii::t('hipanel:domain', 'Check domain') ?>..."/>
         <span class="input-group-btn">
             <button type='submit' id='search-btn' class="btn btn-flat">
                 <i class="fa fa-search"></i>
