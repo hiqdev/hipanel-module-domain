a4f5918e422092ac62bb694290aa5a57eb73f90d
tofid <andreyklochok@gmail.com>
2017-12-08 18:35:35 +0200

    Fixed parking not found image

--- src/views/domain/premium/_formParking.php
+++ src/views/domain/premium/_formParking.php
@@ -20,7 +20,8 @@ $("#parking-type_id").select2({
         return '<img src="https://ahnames.com/www/img/parking/park'+ option.id + '.png" width="50" />&nbsp;&nbsp;' + option.text;
     },
     templateSelection: function (option) {
-        if (option.id.length > 0 ) {
+        console.log(option);
+        if (option.id > 0 ) {
             return '<img src="https://ahnames.com/www/img/parking/park'+ option.id + '.png" width="20" />&nbsp;&nbsp;' + option.text;
         } else {
             return option.text;
