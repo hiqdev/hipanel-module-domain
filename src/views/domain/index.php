7b71b210eec61e80894687cb6121451653d40235
tofid <andreyklochok@gmail.com>
2017-12-14 12:27:27 +0200

    Added `Order certificate` button to domain/index

--- src/views/domain/index.php
+++ src/views/domain/index.php
@@ -3,6 +3,7 @@
 use hipanel\modules\domain\grid\DomainGridView;
 use hipanel\modules\domain\menus\DomainBulkActionsMenu;
 use hipanel\widgets\IndexPage;
+use yii\bootstrap\Html;
 
 /**
  * @var $this \yii\web\View
@@ -17,6 +18,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?php $page = IndexPage::begin(compact('model', 'dataProvider')) ?>
     <?= $page->setSearchFormData() ?>
+
+    <?php $page->beginContent('main-actions') ?>
+        <?php if (Yii::getAlias('@certificate', false)) : ?>
+            <?= Html::a(Yii::t('hipanel:certificate', 'Order certificate'), ['@certificate/order/index'], ['class' => 'btn btn-sm btn-success']) ?>
+        <?php endif ?>
+    <?php $page->endContent() ?>
 
     <?php $page->beginContent('sorter-actions') ?>
         <?= $page->renderSorter([
