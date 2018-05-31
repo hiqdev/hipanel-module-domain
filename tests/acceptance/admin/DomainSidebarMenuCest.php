1ba1a40372e9087cec2bd53120a3e1515369e1f8
pkolomiy <pkolomiy@gmail.com>
2018-05-31 16:49:02 +0300

    Fixed

--- tests/acceptance/admin/DomainSidebarMenuCest.php
+++ tests/acceptance/admin/DomainSidebarMenuCest.php
@@ -4,25 +4,21 @@ namespace hipanel\modules\domain\tests\acceptance\admin;
 
 use hipanel\tests\_support\Page\SidebarMenu;
 use hipanel\tests\_support\Step\Acceptance\Admin;
-use yii\helpers\Url;
 
 class DomainSidebarMenuCest
 {
     public function ensureMenuIsOk(Admin $I)
     {
-        $I->login();
         $menu = new SidebarMenu($I);
 
-        $I->amOnPage(Url::to(['/']));
         $menu->ensureContains('Domains',[
-            'Domains' => '/domain/domain/index',
-            'Name Servers' => '/domain/host/index',
+            'Domains' => '@domain/index',
+            'Name Servers' => '@host/index',
             'Contacts' => '/client/contact/index',
             'WHOIS lookup' => '/domain/whois/index',
             'DNS' => '/dns/zone/index',
         ]);
 
-        $I->amOnPage(Url::to(['/']));
         $menu->ensureDoesNotContain('Domains', [
             'Buy domain',
             'Transfer domain',
--- tests/acceptance/seller/DomainSidebarMenuCest.php
+++ tests/acceptance/seller/DomainSidebarMenuCest.php
@@ -4,19 +4,14 @@ namespace hipanel\modules\domain\tests\acceptance\seller;
 
 use hipanel\tests\_support\Page\SidebarMenu;
 use hipanel\tests\_support\Step\Acceptance\Seller;
-use yii\helpers\Url;
 
 class DomainSidebarMenuCest
 {
     public function ensureMenuIsOk(Seller $I)
     {
-        $I->login();
-        $menu = new SidebarMenu($I);
-
-        $I->amOnPage(Url::to(['/']));
-        $menu->ensureContains('Domains',[
-            'Domains' => '/domain/domain/index',
-            'Name Servers' => '/domain/host/index',
+        (new SidebarMenu($I))->ensureContains('Domains',[
+            'Domains' => '@domain/index',
+            'Name Servers' => '@host/index',
             'Contacts' => '/client/contact/index',
             'WHOIS lookup' => '/domain/whois/index',
             'DNS' => '/dns/zone/index',
