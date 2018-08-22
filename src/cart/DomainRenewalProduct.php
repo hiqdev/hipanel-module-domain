64deec68316b31cc3529e09df61d45adcc05161c
Yurii Myronchuk <bladeroot@gmail.com>
2018-08-22 07:54:38 +0000

    check domain status before renew operation

--- src/cart/DomainRenewalProduct.php
+++ src/cart/DomainRenewalProduct.php
@@ -104,12 +104,29 @@ class DomainRenewalProduct extends AbstractDomainProduct implements BatchPurchas
         return true;
     }
 
+    /**
+     * Checks domains status.
+     *
+     * @param $attribute
+     * @return bool
+     */
+    public function statusValidator($attribute)
+    {
+        if ($this->_model->isRenewable()) {
+            return true;
+        }
+
+        $this->addError('id', Yii::t('hipanel:domain', 'Domain status prohibits this operation'));
+        return false;
+    }
+
     /** {@inheritdoc} */
     public function rules()
     {
         return array_merge(parent::rules(), [
             [['model_id'], 'integer'],
             [['id'], 'daysBeforeExpireValidator'],
+            [['id'], 'statusValidator'],
         ]);
     }
 
