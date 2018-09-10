77a2eabbf895875a461c596ea1647cecdf644b48
Andrey Klochok <andreyklochok@gmail.com>
2018-09-10 16:54:26 +0300

    Minor fixes in design on the domain view page

--- src/views/domain/_premiumTabs.php
+++ src/views/domain/_premiumTabs.php
@@ -3,9 +3,10 @@
 /** @var \hipanel\modules\domain\models\Domain $model */
 /** @var array $forwardingOptions */
 
+use hipanel\modules\domain\assets\PremiumFeatureAsset;
 use hipanel\modules\domain\grid\DomainGridView;
 
-\hipanel\modules\domain\assets\PremiumFeatureAsset::register($this);
+PremiumFeatureAsset::register($this);
 
 $this->registerJs("
 $(document).on('click', '.pf-update-form-close', function () {
@@ -27,42 +28,15 @@ $(document).on('click', '.pf-update-form-close', function () {
 </div>
 
 <div id="url-forwarding" class="tab-pane fade">
-    <div class="panel panel-default">
-        <div class="panel-heading premium-panels-heading">
-            <h3 class="panel-title">
-                <?= Yii::t('hipanel.domain.premium', 'URL forwarding') ?>
-            </h3>
-        </div>
-        <div class="panel-body">
-            <?= $this->render('premium/urlfw', compact('model', 'forwardingOptions')) ?>
-        </div>
-    </div>
+    <?= $this->render('premium/urlfw', compact('model', 'forwardingOptions')) ?>
 </div>
 
 <div id="email-forwarding" class="tab-pane fade">
-    <div class="panel panel-default">
-        <div class="panel-heading premium-panels-heading">
-            <h3 class="panel-title">
-                <?= Yii::t('hipanel.domain.premium', 'Email forwarding') ?>
-            </h3>
-        </div>
-        <div class="panel-body">
-            <?= $this->render('premium/mailfw', ['model' => $model]) ?>
-        </div>
-    </div>
+    <?= $this->render('premium/mailfw', ['model' => $model]) ?>
 </div>
 
 <div id="parking" class="tab-pane fade">
-    <div class="panel panel-default">
-        <div class="panel-heading premium-panels-heading">
-            <h3 class="panel-title">
-                <?= Yii::t('hipanel.domain.premium', 'Parking') ?>
-            </h3>
-        </div>
-        <div class="panel-body">
-            <?= $this->render('premium/parking', ['model' => $model]) ?>
-        </div>
-    </div>
+    <?= $this->render('premium/parking', ['model' => $model]) ?>
 </div>
 
 
