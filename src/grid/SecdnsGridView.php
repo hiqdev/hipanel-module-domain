ba678acf5172c6e89480213f981b68e7027ce890
Yurii Myronchuk <bladeroot@hiqdev.com>
2021-07-14 10:48:06 +0000

    improvement

--- src/grid/SecdnsGridView.php
+++ src/grid/SecdnsGridView.php
@@ -25,54 +25,53 @@ class SecdnsGridView extends BoxedGridView
     {
         return array_merge(parent::columns(), [
             'domain' => [
-                'format' => 'html',
+                'format' => 'raw',
                 'attribute' => 'domain',
                 'filterAttribute' => 'domain_like',
-                'value' => function ($model) {
-
-                    return Html::a($model->domain, DomainController::getActionUrl('view', $model->domain_id));
+                'value' => function ($model): string {
+                    return Html::a(Html::encode($model->domain), DomainController::getActionUrl('view', $model->domain_id));
                 },
             ],
             'key_tag' => [
                 'format' => 'raw',
                 'attribute' => 'key_tag',
-                'value' => function($model) {
-                    return Html::tag('span', $model->key_tag);
+                'value' => function($model): string {
+                    return Html::tag('span', isset($model->key_tag) ? Html::encode($model->key_tag) : '');
                 }
             ],
             'digest_alg' => [
                 'attribute' => 'digest_alg',
                 'format' => 'raw',
-                'value' => function($model) {
+                'value' => function($model): string {
                     return Html::tag('span', $model->getDigestAlgorithm());
                 },
             ],
             'digest_type' => [
                 'attribute' => 'digest_type',
                 'format' => 'raw',
-                'value' => function($model) {
+                'value' => function($model): string {
                     return Html::tag('span', $model->getDigestType());
                 },
             ],
             'digest' => [
                 'attribute' => 'digest',
                 'format' => 'raw',
-                'value' => function($model) {
-                    return Html::tag('span', $model->digest);
+                'value' => function($model): string {
+                    return Html::tag('span', isset($model->digest) ? Html::encode($model->digest) : '');
                 },
             ],
             'key_alg' => [
                 'attribute' => 'key_alg',
                 'format' => 'raw',
-                'value' => function($model) {
+                'value' => function($model): string {
                     return Html::tag('span', $model->getKeyAlgorithm());
                 },
             ],
             'pub_key' => [
                 'attribute' => 'pub_key',
                 'format' => 'raw',
-                'value' => function($model) {
-                    return Html::tag('span', $model->pub_key);
+                'value' => function($model): string {
+                    return Html::tag('span', isset($model->pub_key) ? Html::encode($model->pub_key) '');
                 },
             ],
         ]);
