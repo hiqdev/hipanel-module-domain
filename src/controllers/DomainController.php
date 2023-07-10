a9e6dd57a49ff10a9258669f42cabedb8588d807
Yurii Myronchuk <bladeroot@gmail.com>
2023-07-10 10:18:25 +0200

    AHN-1471: unset grouped models params from request (#136)
    Co-authored-by: Yurii Myronchuk <bladeroot@hiqdev.com>

--- src/controllers/DomainController.php
+++ src/controllers/DomainController.php
@@ -204,7 +204,7 @@ class DomainController extends \hipanel\base\CrudController
                     $receiver = $data['receiver'] ?? null;
                     $withContacts = $data['with_contacts'] ?? null;
                     $skipNotify = !empty($data['skip_notify']) ? Yii::$app->user->can('domain.force-push') : null;
-                    unset($data['pincode'], $data['receiver'], $data['with_contacts']);
+                    unset($data['pincode'], $data['receiver'], $data['with_contacts'], $data['skip_notify']);
                     foreach ($data as &$item) {
                         $item['pincode'] = $pincode;
                         $item['receiver'] = $receiver;
