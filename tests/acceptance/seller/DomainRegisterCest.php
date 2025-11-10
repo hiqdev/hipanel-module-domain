391d7480ecaf79f11a418192538ef1eb4b8385bb
Yurii Myronchuk <bladeroot@hiqdev.com>
2025-11-10 12:14:46 +0000

    AHN-2700: Fix tests

--- tests/acceptance/seller/DomainRegisterCest.php
+++ tests/acceptance/seller/DomainRegisterCest.php
@@ -53,7 +53,7 @@ class DomainRegisterCest
         return [
             [
                 'name' => 'hiqdev',
-                'zone' => '.com',
+                'zone' => '.test',
             ],
         ];
     }
